c70608c10d8d294a4eafcab19b282ff751111ffa
Chris Godber <chrispgodber@gmail.com>
2018-09-04 11:05:18 +0100

    Html to PHP theme integreation
    Modding style to work with sidebar template on PHP Theme - removed 2nd widget area - not required

--- sidebar.php
+++ sidebar.php
@@ -3,12 +3,8 @@
 
 	<?php get_template_part('searchform'); ?>
 
-	<div class="sidebar-widget">
+	<div class="sidebar-widget RightMenu">
 		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-1')) ?>
-	</div>
-
-	<div class="sidebar-widget">
-		<?php if(!function_exists('dynamic_sidebar') || !dynamic_sidebar('widget-area-2')) ?>
 	</div>
 
 </aside>
