c9d3a3352b029f82f526452151170915c4b38de9
Chris Godber <chrispgodber@gmail.com>
2018-09-05 12:16:45 +0100

    added echo for background images on scrollpage template
    addded images for divs on scollpage 
    started adding code to pickup background image
    not showing currently

--- scrollpage.php
+++ scrollpage.php
@@ -15,11 +15,15 @@
        
             <article>
                <?php 
+                $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
                 $pages = get_pages(); 
+                
                 foreach ($pages as $page_data) {
                 $content = apply_filters(‘the_content’, $page_data->post_content); 
                 $title = $page_data->post_title; 
-                echo '<div id = "content">'.$content.'</div>'; 
+               echo  '<div id = "content" style="background: url('.$backgroundImg[0].') no-repeat;">
+                      <div id = "content-area">'.$content.'</div>';
+                  
 }
 ?>
 			</article>
