2363ec04933a69ce81a9cf322ce5c4c4570c8e16
Chris Godber <chrispgodber@gmail.com>
2018-09-05 13:44:35 +0100

    Pulling through images
    added get_the_post_thumbnail_url within for each loop to grab url for each individual page

--- scrollpage.php
+++ scrollpage.php
@@ -18,10 +18,15 @@
                 $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
                 $pages = get_pages(); 
                 
+                $page_id = get_the_ID();
+//                $pages = get_pages('child_of='. $page_id);
+//                
                 foreach ($pages as $page_data) {
                 $content = apply_filters(‘the_content’, $page_data->post_content); 
                 $title = $page_data->post_title; 
-               echo  '<div id = "content" style="background: url('.$backgroundImg[0].') no-repeat;">
+                $images = get_the_post_thumbnail_url($page_data->ID, 'full');  
+                    
+               echo  '<div id = "content" style="background: url('.$images.') no-repeat;">
                       <div id = "content-area">'.$content.'</div>';
                   
 }
