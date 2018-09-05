d9323d641a804ec2f8a1e0c65d5d6ad3bc9bd5f5
Chris Godber <chrispgodber@gmail.com>
2018-09-05 17:27:20 +0100

    rewriting PHP to grab children of page with template
    changing php to grab children of root page
    need to reintegrate background image
    children content displaying ok

--- scrollpage.php
+++ scrollpage.php
@@ -17,34 +17,62 @@
 <!--            <article>-->
                <?php 
                 
-                $page_id = get_the_ID(18); 
-                $args = array( 
-                        'child_of' => $post->$page_id, 
-                        'parent ' => $post->$page_id,
-                        'hierarchical' => 0,
-                        'sort_column' => 'menu_order', 
-                        'sort_order' => 'asc'
-                    );
-                
-                $pages = get_pages($args);
-                
-                $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
-//                $pages = get_pages('child_of='.); 
-                //$pages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
-//               
-//                $pages = get_pages('child_of=',18);
+//                $page_id = get_the_ID(18); 
+//                $args = array( 
+//                        'post_type' => 'page',
+//                        'post_parent'    => $post->$page_id,
+//                        'child_of' => $post->$page_id, 
+//                        'parent ' => $post->$page_id,
+//                        'hierarchical' => 0,
+//                        'sort_column' => 'menu_order', 
+//                        'sort_order' => 'asc'
+//                    );
 //                
-                foreach ($pages as $page_data) {
+//                $my_wp_query = new WP_Query();
+//                $all_wp_pages = $my_wp_query->query(array('post_type' => 'page', 'posts_per_page' => '-1'));
+//                
+//                $about =  get_page_by_title('about');
+//                $about_children = get_page_children( $about->ID, $all_wp_pages );
+////                $pages = get_pages($args);
+//                
+//                $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+////                $pages = get_pages('child_of='.); 
+//                //$pages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
+////               
+////                $pages = get_pages('child_of=',18);
+////                
+//                foreach ($about_children as $page_data) {
+////                $content = apply_filters(‘the_content’, $page_data->post_content); 
 //                $content = apply_filters(‘the_content’, $page_data->post_content); 
-                $content = apply_filters(‘the_content’, $page_data->post_content); 
-                    
-                $title = $page_data->post_title; 
-                $images = get_the_post_thumbnail_url($page_data->ID, 'full');  
-                    
-               echo  
-                      '<div id = "content" style="background: url('.$images.') no-repeat;">
-                       <div id = "content-area">'.$content.'</div></div>';
-                  
+//                $title = $page_data->post_title; 
+//                $images = get_the_post_thumbnail_url($page_data->ID, 'full');  
+//                    
+//               echo   '<pre>' . print_r( $portfolio_children, true ) . '</pre>';
+//                      '<div id = "content" style="background: url('.$images.') no-repeat;">
+//                       <div id = "content-area">'.$title.$content.'</div></div>';
+//                  
+            
+                
+        $childArgs = array(
+            'sort_order' => 'ASC',
+            'sort_column' => 'menu_order',
+            'child_of' => get_the_ID()
+        );
+            
+        $childList = get_pages($childArgs);
+        $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+            
+        $images = get_the_post_thumbnail_url($childList->ID, 'full');  
+            
+        foreach ($childList as $child) { ?>
+            <div class="child-page">
+                <h2 class="child-title"><?php echo $child->post_title; ?></h2>
+                <?php echo apply_filters( 'the_content', $child->post_content); ?>
+            </div>
+        <?php }
+        ?>
+            
+}
 }
 ?>
 <!--			</article>-->
