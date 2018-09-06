51592a8fdf12c518ced84075ae23068afb559dfd
Chris Godber <chrispgodber@gmail.com>
2018-09-06 11:32:10 +0100

    replaced old method with new get child pages

--- scrollpage.php
+++ scrollpage.php
@@ -15,37 +15,28 @@
 		<section>
        <!-- Fetch the pages and display in one continuous scroller -->
 <!--            <article>-->
-               <?php 
+               
+<?php 
                 
-                $page_id = get_the_ID(18); 
-                $args = array( 
-                        'child_of' => $post->$page_id, 
-                        'parent ' => $post->$page_id,
-                        'hierarchical' => 0,
-                        'sort_column' => 'menu_order', 
-                        'sort_order' => 'desc'
-                    );
-                
-                $pages = get_pages($args);
-                
-                $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
-//                $pages = get_pages('child_of='.); 
-                //$pages = get_pages( array( 'child_of' => $post->ID, 'sort_column' => 'post_date', 'sort_order' => 'desc' ) );
-//               
-//                $pages = get_pages('child_of=',18);
-//                
-                foreach ($pages as $page_data) {
-//                $content = apply_filters(‘the_content’, $page_data->post_content); 
-                $content = apply_filters(‘the_content’, $page_data->post_content); 
-                    
-                $title = $page_data->post_title; 
-                $images = get_the_post_thumbnail_url($page_data->ID, 'full');  
-                    
-               echo  
-                      '<div id = "content" style="background: url('.$images.') no-repeat;">
-                       <div id = "content-area">'.$content.'</div></div>';
-                  
-}
+    $childArgs = array( 
+    'sort_order' => 'ASC', 
+    'sort_column' => 'menu_order', 
+    'child_of' => get_the_ID() 
+    ); 
+             
+    $childList = get_pages($childArgs); 
+    $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' ); 
+    $images = get_the_post_thumbnail_url($childList->ID, 'full');  
+            
+    foreach ($childList as $child) {  
+        $images = get_the_post_thumbnail_url($child->ID, ('700,300')); 
+        $content = apply_filters('the_content', $child->post_content);
+        
+        echo  
+            '<div class="content" style="background: url('.$images.') no-repeat;"> 
+            <div id = "content-area">'.$content.'</div></div>'; 
+        } 
+          
 ?>
 <!--			</article>-->
 		</section>
