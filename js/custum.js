7c76cb0d8b823b7812c540c2e51de7b100101ce3
Chris Godber <chrispgodber@gmail.com>
2018-09-06 15:38:16 +0100

    modded scroll added blur to css
    added scroll amount added blur to menu css

--- js/custum.js
+++ js/custum.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
     $( ".scroll" ).click(function() { // Top to Bottom
     console.log("scroll button clicked");
 //    $.scrollTo({bottom:'+=700px'}, 800);
-    $.scrollTo('+=680px', 800);
+    $.scrollTo('+=700px', 800);
     });
 });
